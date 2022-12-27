74dda8ed2a65662ba44c3130cffe14f4032cbd97
Misa <mgomez@ts4.mx>
2022-12-27 02:26:54 -0700

    login diseño adaptado

--- index.php
+++ index.php
@@ -8,83 +8,141 @@ if(isset($_SESSION["user_data"]))
 
 ?>
 
+<!--
+=========================================================
+* Soft UI Dashboard - v1.0.6
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>ConfiguroWeb | GYM</title>
-	<link rel="stylesheet" href="./css/style.css"/>
-	<link rel="stylesheet" type="text/css" href="./css/entypo.css">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="./dashboard/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./dashboard/assets/img/favicon.png">
+  <title>
+    Admin
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="./dashboard/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="./dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="./dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="./dashboard/assets/css/soft-ui-dashboard.css?v=1.0.6" rel="stylesheet" />
 </head>
-<body>
 
-<body class="page-body login-page login-form-fall">
-    	<div id="container">
-			<div class="login-container">
-	
-	<div class="login-header login-caret">
-		
-		<div class="login-content">
-			
-			<a href="#" class="logo">
-				<img src="./images/logo.png" alt="" />
-			</a>
-			
-			<p class="description">ConfiguroWeb | GYM</p>
-			
-			<!-- progress bar indicator -->
-			<div class="login-progressbar-indicator">
-				<h3>43%</h3>
-				<span>Ingresar	...</span>
-			</div>
-		</div>
-		
-	</div>
-	
-	<div class="login-progressbar">
-		<div></div>
-	</div>
-	
-	<div class="login-form">
-		
-		<div class="login-content">
-			
-			<form action="secure_login.php" method='post' id="bb">				
-				<div class="form-group">					
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="entypo-user"></i>
-						</div>
-							<input type="text" placeholder="Usuario" class="form-control" name="user_id_auth" id="textfield" data-rule-minlength="6" data-rule-required="true">
-					</div>
-				</div>				
-								
-				<div class="form-group">					
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="entypo-key"></i>
-						</div>
-						<input type="password" name="pass_key" id="pwfield" class="form-control" data-rule-required="true" data-rule-minlength="6" placeholder="Contraseña">
+<body class="">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent mt-4">
+    <div class="container">
+      <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 text-white" href="./">
+        TitaniumGym Admin
+      </a>
+      <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon mt-2">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </span>
+      </button>
+      
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <main class="main-content  mt-0">
+    <section class="">
+      <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('./dashboard/assets/img/titan.jpg');">
+        <span class="mask bg-gradient-dark opacity-6"></span>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-5 text-center mx-auto">
+              <h1 class="text-white mb-2 mt-5">¡Bienvenido!</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row mt-lg-n10 mt-md-n11 mt-n10">
+          <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+            <div class="card z-index-0">
+              <div class="card-header text-center pt-4">
+                <h5>Iniciar Sesion</h5>
+              </div>
+            
+              <div class="card-body">
+				<form role="form text-left" action="secure_login.php" method='post' id="bb">				
+					<div class="mb-3">				
+						<input type="text" placeholder="Usuario" class="form-control" name="user_id_auth" id="textfield" data-rule-minlength="6" data-rule-required="true">
 					</div>				
-				</div>
+									
+					<div class="mb-3">					
+						<input type="password" name="pass_key" id="pwfield" class="form-control" data-rule-required="true" data-rule-minlength="6" placeholder="Contraseña">			
+					</div>
+					
+					<div class="form-group">
+						<button type="submit" name="btnLogin" class="btn bg-gradient-dark w-100 my-4 mb-2">
+							Ingresar
+							<i class="entypo-login"></i>
+						</button>
+					</div>
+				</form>
 				
-				<div class="form-group">
-					<button type="submit" name="btnLogin" class="btn btn-primary">
-						Ingresar
-						<i class="entypo-login"></i>
-					</button>
-				</div>
-			</form>
-		
-				<div class="login-bottom-links">
-					<a href="forgot_password.php" class="link">¿Olvidaste tu contraseña?</a>
-				</div>			
-		</div>
-		
-	</div>
-	
-</div>
-
-		</div>
-
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+    <footer class="footer py-5">
+      <div class="container">
+        <div class="row">
+        </div>
+        <div class="row">
+          <div class="col-8 mx-auto text-center mt-1">
+            <p class="mb-0 text-secondary">
+              Copyright © <script>
+                document.write(new Date().getFullYear())
+              </script> TitaniumGym.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+  </main>
+  <!--   Core JS Files   -->
+  <script src="./dashboard/assets/js/core/popper.min.js"></script>
+  <script src="./dashboard/assets/js/core/bootstrap.min.js"></script>
+  <script src="./dashboard/assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="./dashboard/assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="./dashboard/assets/js/soft-ui-dashboard.min.js?v=1.0.6"></script>
 </body>
+
 </html>
