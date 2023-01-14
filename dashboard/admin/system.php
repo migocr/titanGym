907a3b0def50384fc8aa62b071471301e2fff43b
Jorge varela <jojovarelaga@ittepic.edu.mx>
2023-01-14 03:46:06 -0600

    SYSTEM

--- dashboard/admin/system.php
+++ dashboard/admin/system.php
@@ -59,32 +59,151 @@
 		<?php $titlePage = 'Resumen'; include 'components/navbar.php'; ?>
 		<!-- End Navbar -->
 		<div class="container-fluid py-4">
-			
-			<div class="row mt-4">
-				<div class="col-lg-12 mb-lg-0 mb-4">
+		
+			<div class="row ">
+				<div class="col-lg-12 col-md-6 mb-md-0 mb-4">
 					<div class="card">
-						<div class="card-body p-3">
+						<div class="card-header pb-0">
+							<div class="">
+								<h6>Systema</h6>
+							</div>
 							<div class="row">
-							<div class="col-12">
-								<div class="card mb-4" style="box-shadow: none;">
-									<div class="card-header pb-0 pt-1">
-									<h6>Systema</h6>
-									<div class="table-responsive p-0">
-										
-										</div>
-									</div>
-									<div class="card-body px-0 pt-0 pb-2">
-									
-									</div>
-								</div>
-								</div>
+								<?php
+	    
+									$query  = "SELECT config FROM system_settings WHERE nombre='color'";
+									$query1  = "SELECT config FROM system_settings WHERE nombre='backgroundColor';";
+									$query2  = "SELECT config FROM system_settings WHERE nombre='nombreSitio';";
+									$query3  = "SELECT config FROM system_settings WHERE nombre='colorFuente';";
+									$query4  = "SELECT config FROM system_settings WHERE nombre='logo'; ;";
+									//echo $query;
+//query-----------------------------------------------------------------------------------------------------
+									$result = mysqli_query($con, $query);
+							
+									if (mysqli_affected_rows($con) == 1) {
+										while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+											$configC    = $row['config'];					
+										}
+									} else{
+										echo "<html><head><script>alert('Cambio Insatisfactorio');</script></head></html>";
+										echo mysqli_error($con);
+									}
+//query1-----------------------------------------------------------------------------------------------------
+									$result1 = mysqli_query($con, $query1);
+									if (mysqli_affected_rows($con) == 1) {
+										while ($row = mysqli_fetch_array($result1, MYSQLI_ASSOC)) {
+											$configBC    = $row['config'];	
+										}
+									} else{
+										echo "<html><head><script>alert('Cambio Insatisfactorio');</script></head></html>";
+										echo mysqli_error($con);
+									}
+//query2-----------------------------------------------------------------------------------------------------
+									$result2 = mysqli_query($con, $query2);
+									if (mysqli_affected_rows($con) == 1) {
+										while ($row = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+											$configNS    = $row['config'];	
+										}
+									} else{
+										echo "<html><head><script>alert('Cambio Insatisfactorio');</script></head></html>";
+										echo mysqli_error($con);
+									}
+//query3-----------------------------------------------------------------------------------------------------
+									$result3 = mysqli_query($con, $query3);
+									if (mysqli_affected_rows($con) == 1) {
+										while ($row = mysqli_fetch_array($result3, MYSQLI_ASSOC)) {
+											$configNC    = $row['config'];	
+										}
+									} else{
+										echo "<html><head><script>alert('Cambio Insatisfactorio');</script></head></html>";
+										echo mysqli_error($con);
+									}
+//query4-----------------------------------------------------------------------------------------------------
+									$result4 = mysqli_query($con, $query4);
+									if (mysqli_affected_rows($con) == 1) {
+										while ($row = mysqli_fetch_array($result4, MYSQLI_ASSOC)) {
+											$configCF    = $row['config'];	
+										}
+									} else{
+										echo "<html><head><script>alert('Cambio Insatisfactorio');</script></head></html>";
+										echo mysqli_error($con);
+									}	
+															
+								?>
+								
+
 							</div>
 						</div>
+						<div class="card" style="background: transparent; box-shadow: none;">
+									<div class="card-body">
+										<div class="row">
+											<div class="col-12">
+												
+												<form id="form1" name="form1" method="post" class="a1-container"
+													action="edit_mem_submit.php">
+													<div class="row">
+														<div class="col-md-6">
+															<div class="form-group">
+																<label for="example-text-input" class="form-control-label">Color Iconos</label>
+																<input class="form-control"  id="boxxe" type="text" name="uid" value="<?php echo $configC?>" readonly required />
+															</div>
+														</div>
+
+													<form id="form1" name="form1" method="post" class="a1-container"
+													action="edit_mem_submit.php">
+													<div class="row">
+														<div class="col-md-6">
+															<div class="form-group">
+																<label for="example-text-input" class="form-control-label">Color background</label>
+																<input class="form-control"  id="boxxe" type="text" name="uid" value="<?php echo $configBC?>" readonly required />
+															</div>
+														</div>
+
+													<form id="form1" name="form1" method="post" class="a1-container"
+													action="edit_mem_submit.php">
+													<div class="row">
+														<div class="col-md-6">
+															<div class="form-group">
+																<label for="example-text-input" class="form-control-label">Nombre del sitio</label>
+																<input class="form-control"  id="boxxe" type="text" name="uid" value="<?php echo $configNS?>" readonly required />
+															</div>
+														</div>
+
+													<form id="form1" name="form1" method="post" class="a1-container"
+													action="edit_mem_submit.php">
+													<div class="row">
+														<div class="col-md-6">
+															<div class="form-group">
+																<label for="example-text-input" class="form-control-label">Color de fuente</label>
+																<input class="form-control"  id="boxxe" type="text" name="uid" value="<?php echo $configNC?>" readonly required />
+															</div>
+														</div>
+
+													<form id="form1" name="form1" method="post" class="a1-container"
+													action="edit_mem_submit.php">
+													<div class="row">
+														<div class="col-md-6">
+															<div class="form-group">
+																<label for="example-text-input" class="form-control-label">Logo</label>
+																<input class="form-control"  id="boxxe" type="text" name="uid" value="<?php echo $configCF?>" readonly required />
+															</div>
+														</div>
+												
+																	
+													
+													<div style="width: auto; display: flex;justify-content: center;">
+														<input class="btn btn-primary" type="submit" name="submit" id="submit" value="Guardar">
+														<input class="btn btn-default" type="reset" name="reset" id="reset" value="Restaurar">
+													</div>
+												</form>
+											</div>
+										</div>
+									</div>
+								</div>
+
 					</div>
 				</div>
 				
 			</div>
-			
 			<?php include 'components/footer.php';?>
 		</div>
 	</main>
@@ -95,7 +214,185 @@
 	<script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
 	<script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
 	<script src="../assets/js/plugins/chartjs.min.js"></script>
-	
+	<script>
+		var ctx = document.getElementById("chart-bars").getContext("2d");
+
+		new Chart(ctx, {
+			type: "bar",
+			data: {
+				labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+				datasets: [{
+					label: "Sales",
+					tension: 0.4,
+					borderWidth: 0,
+					borderRadius: 4,
+					borderSkipped: false,
+					backgroundColor: "#fff",
+					data: [450, 200, 100, 220, 500, 100, 400, 230, 500],
+					maxBarThickness: 6
+				}, ],
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false,
+				plugins: {
+					legend: {
+						display: false,
+					}
+				},
+				interaction: {
+					intersect: false,
+					mode: 'index',
+				},
+				scales: {
+					y: {
+						grid: {
+							drawBorder: false,
+							display: false,
+							drawOnChartArea: false,
+							drawTicks: false,
+						},
+						ticks: {
+							suggestedMin: 0,
+							suggestedMax: 500,
+							beginAtZero: true,
+							padding: 15,
+							font: {
+								size: 14,
+								family: "Open Sans",
+								style: 'normal',
+								lineHeight: 2
+							},
+							color: "#fff"
+						},
+					},
+					x: {
+						grid: {
+							drawBorder: false,
+							display: false,
+							drawOnChartArea: false,
+							drawTicks: false
+						},
+						ticks: {
+							display: false
+						},
+					},
+				},
+			},
+		});
+
+
+		var ctx2 = document.getElementById("chart-line").getContext("2d");
+
+		var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+		gradientStroke1.addColorStop(1, 'rgba(203,12,159,0.2)');
+		gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+		gradientStroke1.addColorStop(0, 'rgba(203,12,159,0)'); //purple colors
+
+		var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+		gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.2)');
+		gradientStroke2.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+		gradientStroke2.addColorStop(0, 'rgba(20,23,39,0)'); //purple colors
+
+		new Chart(ctx2, {
+			type: "line",
+			data: {
+				labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+				datasets: [{
+						label: "Mobile apps",
+						tension: 0.4,
+						borderWidth: 0,
+						pointRadius: 0,
+						borderColor: "#cb0c9f",
+						borderWidth: 3,
+						backgroundColor: gradientStroke1,
+						fill: true,
+						data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+						maxBarThickness: 6
+
+					},
+					{
+						label: "Websites",
+						tension: 0.4,
+						borderWidth: 0,
+						pointRadius: 0,
+						borderColor: "#3A416F",
+						borderWidth: 3,
+						backgroundColor: gradientStroke2,
+						fill: true,
+						data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
+						maxBarThickness: 6
+					},
+				],
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false,
+				plugins: {
+					legend: {
+						display: false,
+					}
+				},
+				interaction: {
+					intersect: false,
+					mode: 'index',
+				},
+				scales: {
+					y: {
+						grid: {
+							drawBorder: false,
+							display: true,
+							drawOnChartArea: true,
+							drawTicks: false,
+							borderDash: [5, 5]
+						},
+						ticks: {
+							display: true,
+							padding: 10,
+							color: '#b2b9bf',
+							font: {
+								size: 11,
+								family: "Open Sans",
+								style: 'normal',
+								lineHeight: 2
+							},
+						}
+					},
+					x: {
+						grid: {
+							drawBorder: false,
+							display: false,
+							drawOnChartArea: false,
+							drawTicks: false,
+							borderDash: [5, 5]
+						},
+						ticks: {
+							display: true,
+							color: '#b2b9bf',
+							padding: 20,
+							font: {
+								size: 11,
+								family: "Open Sans",
+								style: 'normal',
+								lineHeight: 2
+							},
+						}
+					},
+				},
+			},
+		});
+	</script>
+	<script>
+		var win = navigator.platform.indexOf('Win') > -1;
+		if (win && document.querySelector('#sidenav-scrollbar')) {
+			var options = {
+				damping: '0.5'
+			}
+			Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+		}
+	</script>
 	<!-- Github buttons -->
 	<script async defer src="https://buttons.github.io/buttons.js"></script>
 	<!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
@@ -103,3 +400,7 @@
 </body>
 
 </html>
+
+
+
+
